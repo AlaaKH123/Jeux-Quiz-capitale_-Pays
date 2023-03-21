26e445a673a9a53c21f0c6ef1a1a5e41b44943bb
unknown <alakhmiri100@gmail.com>
2023-03-21 22:19:01 +0100

    Final version of the game

---
+++ src/jeux/Test.java
@@ -0,0 +1,13 @@
+package jeux;
+
+public class Test {
+
+	public static void main(String[] args) {
+		GrafiqueQuiz quiz = new GrafiqueQuiz(5);
+		quiz.start();
+		quiz.displayResultats();
+		
+		
+	}
+
+}
